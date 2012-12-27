b040598b323665bdd41737da6848049e84d889d8
Hollin Wilkins <chromaticbum@gmail.com>
2012-12-26 16:50:48 -0800

    module is the global object, unlike in node.

--- src/ev8_require.erl
+++ src/ev8_require.erl
@@ -65,9 +65,8 @@ cache_miss(_, Vm, ModuleFile) ->
 cache_miss_js(Vm, ModuleFile) ->
   C = ev8:new_context(Vm),
   ev8:install(C, [ev8_erlang, ev8_require]),
-  Module = ev8:eval(C, <<"new Object">>),
   Exports = ev8:eval(C, <<"new Object">>),
-  ev8:set(C, global, [{<<"module">>, Module},
+  ev8:set(C, global, [{<<"module">>, global},
                       {<<"exports">>, Exports}]),
   ev8cache:insert(Vm, {ev8_require, ModuleFile}, C),
   ev8:eval_file(C, ModuleFile),
